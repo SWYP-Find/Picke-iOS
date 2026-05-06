0454cb422e981d3475b3976c091764604180d295
Roy <shuwj81@daum.net>
2026-05-07 02:17:13 +0900

    chore: DEBUG 전용 TCA 로그 제한 #1
    - PickeApp
      - AppReducer printChanges 호출을 DEBUG 빌드에서만 실행하도록 분기
      - Release 계열 빌드에서는 기본 AppReducer만 생성하도록 정리

--- Projects/App/Sources/Application/PickeApp.swift
+++ Projects/App/Sources/Application/PickeApp.swift
@@ -12,9 +12,13 @@ struct PickeApp: App {
   var body: some Scene {
     WindowGroup {
       let store = Store(initialState: AppReducer.State()) {
+#if DEBUG
         AppReducer()
           ._printChanges()
           ._printChanges(.actionLabels)
+#else
+        AppReducer()
+#endif
       }
       
       AppView(store: store)
