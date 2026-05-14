4379fd28c5357969a05415ed3c78571d679db0ae
Roy <shuwj81@daum.net>
2026-05-14 17:04:10 +0900

    chore: 카카오 OAuth 파일 들여쓰기를 프로젝트 컨벤션(2-space) 으로 정렬 #20
    KakaoOAuthRepositoryProtocol / KakaoAuthCodeStore 가 4-space 로 들어와 있어 다른 파일들과
    스타일이 어긋났다. 동작 변경 없이 들여쓰기만 2-space 로 통일한다.

--- Projects/Domain/DomainInterface/Sources/Kakao/KakaoOAuthRepositoryProtocol.swift
+++ Projects/Domain/DomainInterface/Sources/Kakao/KakaoOAuthRepositoryProtocol.swift
@@ -5,26 +5,28 @@
 //  Created by Assistant on 12/4/25.
 //
 
-import Foundation
-import Entity
 import Dependencies
+import Entity
+import Foundation
 
 public protocol KakaoOAuthInterface: Sendable {
-    func signIn() async throws -> KakaoOAuthPayload
+  func signIn() async throws -> KakaoOAuthPayload
 }
 
 // MARK: - Dependencies
+
 public struct KakaoOAuthRepositoryDependencyKey: DependencyKey {
-    public static var liveValue: KakaoOAuthInterface {
-        fatalError("KakaoOAuthRepositoryDependency liveValue not implemented")
-    } 
-    public static var previewValue: KakaoOAuthInterface = MockKakaoOAuthRepository()
-    public static var testValue: KakaoOAuthInterface = MockKakaoOAuthRepository()
+  public static var liveValue: KakaoOAuthInterface {
+    fatalError("KakaoOAuthRepositoryDependency liveValue not implemented")
+  }
+
+  public static var previewValue: KakaoOAuthInterface = MockKakaoOAuthRepository()
+  public static var testValue: KakaoOAuthInterface = MockKakaoOAuthRepository()
 }
 
 public extension DependencyValues {
-    var kakaoOAuthRepository: KakaoOAuthInterface {
-        get { self[KakaoOAuthRepositoryDependencyKey.self] }
-        set { self[KakaoOAuthRepositoryDependencyKey.self] = newValue }
-    }
+  var kakaoOAuthRepository: KakaoOAuthInterface {
+    get { self[KakaoOAuthRepositoryDependencyKey.self] }
+    set { self[KakaoOAuthRepositoryDependencyKey.self] = newValue }
+  }
 }
