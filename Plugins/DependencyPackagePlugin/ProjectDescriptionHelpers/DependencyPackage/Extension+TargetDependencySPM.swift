70cd918b0ec0f7b2412e3c31ad2574b34b36bf1b
Roy <shuwj81@daum.net>
2026-05-16 00:37:02 +0900

    chore: #3 Kingfisher SPM 의존성 + HomeRepositoryImpl DI 등록
    - Extension+TargetDependencySPM 에 kingfisher 외부 의존성 등록
    - Home 프레젠테이션 모듈이 Kingfisher 를 참조하도록 Project.swift 갱신
    - AppDIManager 에서 HomeInterface ↔ HomeRepositoryImpl 바인딩

--- Plugins/DependencyPackagePlugin/ProjectDescriptionHelpers/DependencyPackage/Extension+TargetDependencySPM.swift
+++ Plugins/DependencyPackagePlugin/ProjectDescriptionHelpers/DependencyPackage/Extension+TargetDependencySPM.swift
@@ -10,15 +10,16 @@ import ProjectDescription
 public extension TargetDependency.SPM {
   static let asyncMoya = TargetDependency.external(name: "AsyncMoya", condition: .none)
   static let logMarco = TargetDependency.external(name: "LogMacro", condition: .none)
-  
+
   static let composableArchitecture = TargetDependency.external(name: "ComposableArchitecture", condition: .none)
   static let dependencies = TargetDependency.external(name: "Dependencies", condition: .none)
   static let identifiedCollections = TargetDependency.external(name: "IdentifiedCollections", condition: .none)
   static let tcaFlow = TargetDependency.external(name: "TCAFlow", condition: .none)
   static let concurrencyExtras = TargetDependency.external(name: "ConcurrencyExtras", condition: .none)
   static let sdwebImage = TargetDependency.external(name: "SDWebImageSwiftUI", condition: .none)
+  static let kingfisher = TargetDependency.external(name: "Kingfisher", condition: .none)
   static let weaveDI = TargetDependency.external(name: "WeaveDI", condition: .none)
-  
+
   static let googleSignIn = TargetDependency.external(name: "GoogleSignIn", condition: .none)
   static let appAuth: TargetDependency = .external(name: "AppAuth")
   static let firebaseCrashlytics = TargetDependency.external(name: "FirebaseCrashlytics", condition: .none)
@@ -27,5 +28,4 @@ public extension TargetDependency.SPM {
   static let googleMobileAds = TargetDependency.external(name: "GoogleMobileAds", condition: .none)
   static let mixpanel = TargetDependency.external(name: "Mixpanel", condition: .none)
   static let mixpanelSessionReplay = TargetDependency.external(name: "MixpanelSessionReplay", condition: .none)
-  
 }
--- Projects/Presentation/Home/Project.swift
+++ Projects/Presentation/Home/Project.swift
@@ -1,8 +1,8 @@
+import DependencyPackagePlugin
+import DependencyPlugin
 import Foundation
 import ProjectDescription
-import DependencyPlugin
 import ProjectTemplatePlugin
-import DependencyPackagePlugin
 
 let project = Project.makeAppModule(
   name: "Home",
@@ -12,6 +12,7 @@ let project = Project.makeAppModule(
   dependencies: [
     .SPM.logMarco,
     .SPM.tcaFlow,
+    .SPM.kingfisher,
     .Domain(implements: .UseCase),
     .Shared(implements: .DesignSystem),
   ],
