328d98316f76ad7d67761440e1160322c75cf202
Roy <shuwj81@daum.net>
2026-06-06 01:31:12 +0900

    feat: DesignSystem — vs(versus_home) 에셋 + UIColor.gray200 토큰 추가 / 온보딩 svg 교체 #4

--- Projects/Shared/DesignSystem/Sources/Extension/Color/UIColor+.swift
+++ Projects/Shared/DesignSystem/Sources/Extension/Color/UIColor+.swift
@@ -19,12 +19,13 @@ public extension UIColor {
     scanner.scanHexInt64(&rgb)
 
     let r = Double((rgb >> 16) & 0xFF) / 255.0
-    let g = Double((rgb >>  8) & 0xFF) / 255.0
-    let b = Double((rgb >>  0) & 0xFF) / 255.0
+    let g = Double((rgb >> 8) & 0xFF) / 255.0
+    let b = Double((rgb >> 0) & 0xFF) / 255.0
     self.init(red: r, green: g, blue: b, alpha: alpha)
   }
 
   static var neutral900: UIColor { .init(hex: "131212") }
+  static var gray200: UIColor { .init(hex: "B0AFAE") }
   static var bgDefault: UIColor { .init(hex: "FAFAF9") }
   static var borderDefault: UIColor { .init(hex: "EFEAE0") }
 }
