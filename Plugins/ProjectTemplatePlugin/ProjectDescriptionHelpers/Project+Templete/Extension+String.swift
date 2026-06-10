9c92602c1bf10c5d3e7ad20289e46485f675f667
Roy <shuwj81@daum.net>
2026-06-10 23:14:27 +0900

    chore: 빌드 버전 22 → 23

--- Plugins/ProjectTemplatePlugin/ProjectDescriptionHelpers/Project+Templete/Extension+String.swift
+++ Plugins/ProjectTemplatePlugin/ProjectDescriptionHelpers/Project+Templete/Extension+String.swift
@@ -17,7 +17,7 @@ extension String {
     return Project.Environment.bundlePrefix
   }
   
-  public static func appBuildVersion(buildVersion: String = "22") -> String {
+  public static func appBuildVersion(buildVersion: String = "23") -> String {
     return buildVersion
   }
   
