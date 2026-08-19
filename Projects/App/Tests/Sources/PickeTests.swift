394d0e341438398613ccdf5c97a30277ee372303
Roy <shuwj81@daum.net>
2026-08-19 21:43:10 +0900

    style(app): 앱 테스트 코드 들여쓰기·개행 정리
    동작 변경 없음. 2-space 들여쓰기와 파일 끝 개행만 맞춘다.

--- Projects/App/Tests/Sources/PickeTests.swift
+++ Projects/App/Tests/Sources/PickeTests.swift
@@ -8,24 +8,22 @@
 import XCTest
 
 final class PickeTests: XCTestCase {
+  override func setUpWithError() throws {
+    // Put setup code here.
+  }
 
-    override func setUpWithError() throws {
-        // Put setup code here.
+  override func tearDownWithError() throws {
+    // Put teardown code here.
+  }
+
+  func testExample() throws {
+    // This is an example of a functional test case.
+  }
+
+  func testPerformanceExample() throws {
+    // This is an example of a performance test case.
+    measure {
+      // Put the code you want to measure the time of here.
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  }
 }
