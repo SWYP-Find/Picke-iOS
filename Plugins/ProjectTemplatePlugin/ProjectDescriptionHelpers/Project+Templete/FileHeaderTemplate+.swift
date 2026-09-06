02e3a7225d9076f86468d84c07cb97d188693a36
Roy <suhwj81@gmail.com>
2026-09-06 20:53:19 +0900

    chore(tuist): 파일 헤더 템플릿에 저작권 줄을 추가해 DDD 형식과 맞춘다
    날짜는 이미 DDD 와 동일하게 ___DATE___ (Xcode 가 파일 생성 시점을 채움) 를
    쓰고 있었고, 빠져 있던 Copyright 줄만 채웠다.
    
    검증: tuist generate 후 생성된 IDETemplateMacros.plist 에 반영 확인.

--- Plugins/ProjectTemplatePlugin/ProjectDescriptionHelpers/Project+Templete/FileHeaderTemplate+.swift
+++ Plugins/ProjectTemplatePlugin/ProjectDescriptionHelpers/Project+Templete/FileHeaderTemplate+.swift
@@ -3,14 +3,14 @@
 //  ProjectDescriptionHelpers
 //
 //  Xcode 파일 헤더(IDETemplateMacros). 새 파일 생성 시 자동 삽입된다.
-//  ___FILENAME___/___PACKAGENAME___/___DATE___ 는 Xcode 가 채운다.
+//  ___FILENAME___/___PACKAGENAME___/___DATE___/___YEAR___ 는 Xcode 가 채운다.
 //  작성자는 기여자와 무관하게 프로젝트명으로 고정한다.
 //
 
 import ProjectDescription
 
 public extension FileHeaderTemplate {
-  /// 프로젝트 공통 파일 헤더.
+  /// 프로젝트 공통 파일 헤더 (저작권 포함).
   static var `default`: FileHeaderTemplate {
     """
     //
@@ -18,6 +18,7 @@ public extension FileHeaderTemplate {
     //  ___PACKAGENAME___
     //
     //  Created by Picke on ___DATE___.
+    //  Copyright © ___YEAR___ Picke. All rights reserved.
     //
     """
   }
