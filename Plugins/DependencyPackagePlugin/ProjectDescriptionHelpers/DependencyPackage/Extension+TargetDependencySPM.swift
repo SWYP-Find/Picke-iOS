0059cc07d5aa65ce32a3d1a013372d89ff59fc2e
Roy <suhwj81@gmail.com>
2026-09-07 13:01:30 +0900

    chore(tuist): 모듈 카탈로그와 패키지 의존성을 갱신한다

--- Plugins/DependencyPackagePlugin/ProjectDescriptionHelpers/DependencyPackage/Extension+TargetDependencySPM.swift
+++ Plugins/DependencyPackagePlugin/ProjectDescriptionHelpers/DependencyPackage/Extension+TargetDependencySPM.swift
@@ -13,6 +13,8 @@ public extension TargetDependency.SPM {
 
   static let composableArchitecture = TargetDependency.external(name: "ComposableArchitecture", condition: .none)
   static let dependencies = TargetDependency.external(name: "Dependencies", condition: .none)
+  static let sharing = TargetDependency.external(name: "Sharing", condition: .none)
+  static let sqliteData = TargetDependency.external(name: "SQLiteData", condition: .none)
   static let identifiedCollections = TargetDependency.external(name: "IdentifiedCollections", condition: .none)
   static let tcaFlow = TargetDependency.external(name: "TCAFlow", condition: .none)
   static let concurrencyExtras = TargetDependency.external(name: "ConcurrencyExtras", condition: .none)
--- Plugins/ProjectTemplatePlugin/ProjectDescriptionHelpers/Project+Templete/Project+Target.swift
+++ Plugins/ProjectTemplatePlugin/ProjectDescriptionHelpers/Project+Templete/Project+Target.swift
@@ -9,7 +9,10 @@ import ProjectDescription
 
 let suppressWarningsSettings: ProjectDescription.Settings = .settings(
   base: [
-    "OTHER_SWIFT_FLAGS": "$(inherited) -suppress-warnings",
+    // Xcode 26 의 XCTest 가 먼저 로드하는 애플 private `Sharing` 모듈과 충돌해 xctest 부팅이 깨진다.
+    // Point-Free 구현은 `PickePointFreeSharing` 이름으로 빌드하고(Tuist/Package.swift),
+    // 소스의 `import Sharing` 은 별칭으로 이어 붙인다.
+    "OTHER_SWIFT_FLAGS": "$(inherited) -suppress-warnings -module-alias Sharing=PickePointFreeSharing",
     // Xcode 16 Explicitly Built Modules 비활성화.
     // (system 모듈(os_object)/WebKit pcm emit 실패 및 "implicit use of module files is disabled" 에러 회피)
     "SWIFT_ENABLE_EXPLICIT_MODULES": "NO",
